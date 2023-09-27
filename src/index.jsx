ecb8c508665f1c1241623655ff864b663e7d6b03
Mladen <mladenmilosevic1808@gmail.com>
2023-09-27 14:51:16 +0200

    books-pics

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,10 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "./context/books";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <Provider>
     <App />
-  </Provider>
 );
